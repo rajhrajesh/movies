ca75e93e484c4d0dbd2f6654e64fe2eb992d7275
Rajesh P <160295019+rajhrajesh@users.noreply.github.com>
2024-03-23 18:42:19 +0530

    final correction to revison

--- correct in app.js
+++ correct in app.js
@@ -1,139 +1,160 @@
 const express = require('express')
+const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
-const path = require('path')
-
-const databasePath = path.join(__dirname, 'moviesData.db')
 
 const app = express()
-
 app.use(express.json())
+const dbPath = path.join(__dirname, 'goodreads.db')
 
-let database = null
+let db = null
 
-const initializeDbAndServer = async () => {
+const initializeDBAndServer = async () => {
   try {
-    database = await open({
-      filename: databasePath,
+    db = await open({
+      filename: dbPath,
       driver: sqlite3.Database,
     })
-    app.listen(3000, () =>
-      console.log('Server Running at http://localhost:3000/'),
-    )
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`)
+    app.listen(3000, () => {
+      console.log('Server Running at http://localhost:3000/')
+    })
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`)
     process.exit(1)
   }
 }
+initializeDBAndServer()
 
-initializeDbAndServer()
-
-const convertMovieDbObjectToResponseObject = dbObject => {
-  return {
-    movieId: dbObject.movie_id,
-    directorId: dbObject.director_id,
-    movieName: dbObject.movie_name,
-    leadActor: dbObject.lead_actor,
-  }
-}
-
-const convertDirectorDbObjectToResponseObject = dbObject => {
-  return {
-    directorId: dbObject.director_id,
-    directorName: dbObject.director_name,
-  }
-}
-
-app.get('/movies/', async (request, response) => {
-  const getMoviesQuery = `
-    SELECT
-      movie_name
-    FROM
-      movie;`
-  const moviesArray = await database.all(getMoviesQuery)
-  response.send(
-    moviesArray.map(eachMovie => ({movieName: eachMovie.movie_name})),
-  )
-})
-
-app.get('/movies/:movieId/', async (request, response) => {
-  const {movieId} = request.params
-  const getMovieQuery = `
-    SELECT 
-      *
-    FROM 
-      movie 
-    WHERE 
-      movie_id = ${movieId};`
-  const movie = await database.get(getMovieQuery)
-  response.send(convertMovieDbObjectToResponseObject(movie))
-})
-
-app.post('/movies/', async (request, response) => {
-  const {directorId, movieName, leadActor} = request.body
-  const postMovieQuery = `
-  INSERT INTO
-    movie ( director_id, movie_name, lead_actor)
-  VALUES
-    (${directorId}, '${movieName}', '${leadActor}');`
-  await database.run(postMovieQuery)
-  response.send('Movie Successfully Added')
-})
-
-app.put('/movies/:movieId/', async (request, response) => {
-  const {directorId, movieName, leadActor} = request.body
-  const {movieId} = request.params
-  const updateMovieQuery = `
-            UPDATE
-              movie
-            SET
-              director_id = ${directorId},
-              movie_name = '${movieName}',
-              lead_actor = '${leadActor}'
-            WHERE
-              movie_id = ${movieId};`
-
-  await database.run(updateMovieQuery)
-  response.send('Movie Details Updated')
-})
-
-app.delete('/movies/:movieId/', async (request, response) => {
-  const {movieId} = request.params
-  const deleteMovieQuery = `
-  DELETE FROM
-    movie
-  WHERE
-    movie_id = ${movieId};`
-  await database.run(deleteMovieQuery)
-  response.send('Movie Removed')
-})
-
-app.get('/directors/', async (request, response) => {
-  const getDirectorsQuery = `
+// Get Books API
+app.get('/books/', async (request, response) => {
+  const {
+    offset = 2,
+    limit = 5,
+    order = 'ASC',
+    order_by = 'book_id',
+    search_q = '',
+  } = request.query
+  const getBooksQuery = `
     SELECT
       *
     FROM
-      director;`
-  const directorsArray = await database.all(getDirectorsQuery)
-  response.send(
-    directorsArray.map(eachDirector =>
-      convertDirectorDbObjectToResponseObject(eachDirector),
-    ),
-  )
-})
-
-app.get('/directors/:directorId/movies/', async (request, response) => {
-  const {directorId} = request.params
-  const getDirectorMoviesQuery = `
-    SELECT
-      movie_name
-    FROM
-      movie
+     book
     WHERE
-      director_id='${directorId}';`
-  const moviesArray = await database.all(getDirectorMoviesQuery)
-  response.send(
-    moviesArray.map(eachMovie => ({movieName: eachMovie.movie_name})),
-  )
+     title LIKE '%${search_q}%'
+    ORDER BY ${order_by} ${order}
+    LIMIT ${limit} OFFSET ${offset};`
+  const booksArray = await db.all(getBooksQuery)
+  response.send(booksArray)
 })
-module.exports = app
+
+//Get Book API
+app.get('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const getBookQuery = `SELECT
+      *
+    FROM
+      book
+    WHERE
+      book_id = ${bookId};`
+  const book = await db.get(getBookQuery)
+  response.send(book)
+})
+
+//Post Book API
+app.post('/books/', async (request, response) => {
+  const bookDetails = request.body
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+  const addBookQuery = `INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`
+
+  const dbResponse = await db.run(addBookQuery)
+  const bookId = dbResponse.lastID
+  response.send({bookId: bookId})
+})
+
+//Put Book API
+app.put('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const bookDetails = request.body
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+  const updateBookQuery = `UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price=${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`
+  await db.run(updateBookQuery)
+  response.send('Book Updated Successfully')
+})
+
+//Delete Book API
+app.delete('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const deleteBookQuery = `DELETE FROM 
+      book 
+    WHERE
+      book_id = ${bookId};`
+  await db.run(deleteBookQuery)
+  response.send('Book Deleted Successfully')
+})
+
+//Get Author Books API
+app.get('/authors/:authorId/books/', async (request, response) => {
+  const {authorId} = request.params
+  const getAuthorBooksQuery = `SELECT
+     *
+    FROM
+     book
+    WHERE
+      author_id = ${authorId};`
+  const booksArray = await db.all(getAuthorBooksQuery)
+  response.send(booksArray)
+})
